74fcffc782276c9664a6d3e424f0342275fb8c8b
Michael438-sudo <michael.robinson26@compscihigh.org>
2024-09-18 15:11:45 +0000

    Message in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log("michael robinson")
 // Stage and commit this change with an appropriate message
 // Task 2
 // Print your favorite snack
-
+console.log("Ice cream")
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
